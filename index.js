9ff6f3a67ca14977a362e530fd652b4721e88243
Nathan Hermes <39421934+NathanHermes@users.noreply.github.com>
2022-01-04 18:14:21 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
   $("input").change(function (event) {
     let value = event.target.value;
-
-    $("td:nth-child(1)").text((value / 1.618) / 1.618));
+    
+    $("td:nth-child(1)").text(value / 1.618 / 1.618);
     $("td:nth-child(2)").text((value / 1.618).toFixed(2));
     $("td:nth-child(3)").text(value);
     $("td:nth-child(4)").text((value * 1.618).toFixed(2));
